a636a4646cb430c6719145c31cf3dad85f3713ea
theeugenechong <eugenechong20011@gmail.com>
2022-09-15 14:44:11 +0800

    Remove Tiktok playlist

--- config/playlists.py
+++ config/playlists.py
@@ -5,8 +5,7 @@ spotify = spotipy.Spotify(client_credentials_manager=SpotifyClientCredentials())
 
 def target_playlists():
     playlists = {
-        'viral_hits': 'spotify:playlist:37i9dQZF1DX2L0iB23Enbq',
-        'tiktok_songs_2022': 'spotify:playlist:65LdqYCLcsV0lJoxpeQ6fW'
+        'viral_hits': 'spotify:playlist:37i9dQZF1DX2L0iB23Enbq'
     }
     return playlists
 
